4dfdfbd538cf88a24f5a1a4828b57d711d97faba
ge1118 <greean246@gmail.com>
2024-01-31 18:08:24 -0500

    update resume

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -4,6 +4,9 @@ import './Resume.scss'
 const Resume = () => {
     return (
         <div className="resume">
+            <a href="/Resume.pdf" download="Leah K. - Resume.pdf" className="download-btn">
+                Download My Resume
+            </a>
             <iframe src="/Resume.pdf" height='fit-content' />
         </div>
     )
